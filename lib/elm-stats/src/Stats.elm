6b6d9f659a90d9869998f87f2d86f7000c655dc1
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-10-16 21:13:41 -0700

    Add Stats.bins

--- lib/elm-stats/src/Stats.elm
+++ lib/elm-stats/src/Stats.elm
@@ -1,7 +1,11 @@
 module Stats
     ( normal
     , stddev
+    , bins
     ) where
+
+
+import Dict
 
 
 type alias ContinuousDistribution =
@@ -37,3 +41,35 @@ stddev xs =
         List.foldl step (0,0,0) xs
         |> (\(_,q,n) -> q/(n-1))
         |> sqrt
+
+
+bins : List Float -> List (Float, Float)
+bins data =
+    let
+        n = List.length data
+        sigma = stddev data
+        binSize = 3.5 * sigma / ((toFloat n)^(1.0/3))
+    in
+        bins' binSize data
+
+
+bins' : Float -> List (Float) -> List (Float, Float)
+bins' size data =
+    let
+        min = List.minimum data |> Maybe.withDefault 0
+        max = List.maximum data |> Maybe.withDefault (min+1)
+        step list dict =
+            case list of
+                [] ->
+                    dict
+                (next::rest) ->
+                    let
+                        index = floor <| (next-min) / size
+                        inc = (+) 1
+                        d' = Dict.update index (Maybe.withDefault 0 >> inc >> Just) dict
+                    in
+                        step rest d'
+    in
+        step data Dict.empty
+        |> Dict.toList
+        |> List.map (\(x,y) -> ((toFloat x)*size+min,y))
--- lib/elm-stats/tests/StatsTest.elm
+++ lib/elm-stats/tests/StatsTest.elm
@@ -28,5 +28,20 @@ all =
       [ 0,1 ]
       |> Stats.stddev
       |> assertEqual 0.7071067811865476
-      |> test "standard deviation"
+      |> test "standard deviation",
+
+      [1,2,3,4,5]
+      |> Stats.bins
+      |> assertEqual [(1,4),(4.236294590020318,1)]
+      |> test "trivial binning",
+
+      [1,2]
+      |> Stats.bins
+      |> assertEqual [(1,2)]
+      |> test "trivial binning fits the range",
+
+      [1,2,3,5,6]
+      |> Stats.bins
+      |> assertEqual [(1,4),(5.244360564117875,1)]
+      |> test "simple binning"
     ]
